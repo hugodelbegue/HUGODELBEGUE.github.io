0e1f9edd5fce2c9995d1c36b0bbf2825904553a6
hugodelbegue <hugodelbegue@gmail.com>
2023-10-26 23:02:29 +0200

    fix ARCHIBALD upgrade 3

--- euroexpo/archibald/assets/ProjectsDisplay-13d9769c.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as c,c as o,p as t,b as _,d as a}from"./index-4520eff0.js";const d={},p=e=>(t("data-v-cb13559d"),e=e(),_(),e),n={class:"wrapper-projects-display middle"},r=p(()=>a("div",{class:"projects-display-content"}," PROJECTS ",-1)),i=[r];function l(e,h){return c(),o("section",n,i)}const f=s(d,[["render",l],["__scopeId","data-v-cb13559d"]]);export{f as P};
