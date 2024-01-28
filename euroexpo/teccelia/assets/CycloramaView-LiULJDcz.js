b736118f944fb597c23b771f66eb1bf596f5b9ee
hugodelbegue <hugodelbegue@gmail.com>
2024-01-29 00:25:04 +0100

    TECCELIA add popup infos - optimiz maquette render

---
+++ euroexpo/teccelia/assets/CycloramaView-LiULJDcz.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-TIV-1VpC.js";const s={},t={class:"middle global-padding"};function a(n,r){return o(),c("section",t," CYCLORAMA ")}const l=e(s,[["render",a]]);export{l as default};
--- euroexpo/teccelia/assets/VisitView-wxGaqw_8.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-P4mT-UQf.js";const s={},t={class:"middle global-padding"};function n(a,r){return o(),c("section",t," VISITE APPARTEMENT ")}const i=e(s,[["render",n]]);export{i as default};
