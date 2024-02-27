c6edb709b64ebc5c3bff01239c8203b343d3097b
hugodelbegue <hugodelbegue@gmail.com>
2024-02-27 12:42:55 +0100

    TECCELIA - Upgrade

---
+++ euroexpo/teccelia/assets/3DView-6mWmpEmz.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-akOvEY_-.js";const s={},t={class:"wrapper-3D middle"};function r(n,_){return o(),c("section",t," PLAN 3D ")}const i=e(s,[["render",r]]);export{i as default};
