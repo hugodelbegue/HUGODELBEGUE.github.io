47c1a3eb916905b607a9f6ed91b86cce1221c93e
hugodelbegue <hugodelbegue@gmail.com>
2024-03-01 13:15:43 +0100

    TECCELIA - fix bugs

--- euroexpo/teccelia/assets/VisitView-ikVRJpIr.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-7xhF86hp.js";const s={},t={class:"middle global-padding"};function n(a,r){return o(),c("section",t," VISITE APPARTEMENT ")}const i=e(s,[["render",n]]);export{i as default};
